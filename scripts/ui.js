287794506f927bd34a2eb61f8304ed0d8ce4a925
tjgillweb <tjgillweb@gmail.com>
2020-04-17 22:13:41 +0530

    Removed dateFns library and dependencies

--- scripts/ui.js
+++ scripts/ui.js
@@ -7,16 +7,12 @@ class ChatUI{
     }
     //render chat templates to the DOM
     render(data){
-        const when = dateFns.distanceInWordsToNow(
-            data.created_at.toDate(),
-            {addSuffix: true}
-        );
         const html=`
             <li class="list-group-item">
                 <img src="images/man.svg">
                 <span class="username">${data.username}</span>
                 <span class="message">${data.message}</span>
-                <div class="time">${when} </div>
+                <div class="time">${String(data.created_at.toDate()).substr(16,5)} </div>
             </li>
         `;
         this.list.innerHTML += html;
